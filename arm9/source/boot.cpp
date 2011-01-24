c1a9e9c1b6de60dc9fa5be8546febc5e01576645
Flare183 <flare@flare183.org>
2011-01-24 00:37:28 -0500

    Cleanup and such

--- arm9/source/boot.cpp
+++ arm9/source/boot.cpp
@@ -16,7 +16,7 @@
  *  along with DSOrganize.  If not, see <http://www.gnu.org/licenses/>.    *
  *                                                                         *
  ***************************************************************************/
- 
+
 #include <nds.h>
 #include <string.h>
 #include <stdio.h>
@@ -37,46 +37,46 @@
 void exec(char *command, bool gbaBoot, bool memBoot)
 {
 	strlwr(command);
-	
+
 	// "..." ignored atm
-	// Set up VRAM, to put the binary in. 
+	// Set up VRAM, to put the binary in.
 	//  Bank A might be used for text out (will be used by the stub)
 	//  Bank B,D to carry over param&binary
-    vramSetMainBanks(   VRAM_A_MAIN_BG_0x6000000,       
-                        VRAM_B_MAIN_BG_0x6020000,      
-                        VRAM_C_LCD,  
-                        VRAM_D_MAIN_BG_0x6040000   
-                        );    
-						
+    vramSetMainBanks(   VRAM_A_MAIN_BG_0x6000000,
+			VRAM_B_MAIN_BG_0x6020000,
+			VRAM_C_LCD,
+			VRAM_D_MAIN_BG_0x6040000
+			);
+
 	//set the video mode
     videoSetMode(  MODE_0_2D | DISPLAY_BG0_ACTIVE );
 	// black backdrop
 	BG_PALETTE[0]=RGB15(0,0,0);
 	BG0_CR = BG_MAP_BASE(31);
 	BG_PALETTE[255] = RGB15(31,31,31);
-	// set up debug output 
-	
+	// set up debug output
+
 	// you can load the stub from whatever place: gbfs,wifi,fat & whatever
 	DRAGON_chdir("/");
 	char cCommand[256];
 	sprintf(cCommand, "%sexec_stub.bin", d_res);
 	DRAGON_FILE *stub = DRAGON_fopen(cCommand,"r");
 	u32 tLen = DRAGON_flength(stub);
-	
+
 	long *buffer = (long *)safeMalloc(tLen);
-	
+
 	DRAGON_fread(buffer,tLen,1,stub) ;
 	DRAGON_fclose(stub) ;
-	
+
 	dldiPatchLoader((data_t *)buffer, tLen);
-	
+
 	// we copy wordwise, as bytewise write access to VRAM is a bad idea
 	u32 i ;
 	long *target = (long *)0x6020000 ;
 	for (i=0;i<tLen >> 2;i++) {
 		target[i] = buffer[i] ;
 	} ;
-	
+
 	if(memBoot)
 	{
 		*(vu32 *)0x0605FFFC = (unsigned int)command;
@@ -92,7 +92,7 @@ void exec(char *command, bool gbaBoot, bool memBoot)
 	}
 
 	u32 tBoot = LF_LOADARM9 | LF_LOADARM7 | LF_CLEARMEM | LF_LOADGBFS;
-	
+
 	if(memBoot)
 		tBoot |= LF_MEMDIRECT;
 	else
@@ -100,13 +100,12 @@ void exec(char *command, bool gbaBoot, bool memBoot)
 		if(gbaBoot)
 			tBoot |= LF_GBABOOT;
 	}
-	
+
 	*(unsigned long *)0x0605FFF8 = tBoot;
-	
+
 	typedef void (*stub_type)(void) ;
 
 	stub_type stb = (stub_type)0x06020000 ;
 	stb() ;
-	
-} ;
 
+} ;
